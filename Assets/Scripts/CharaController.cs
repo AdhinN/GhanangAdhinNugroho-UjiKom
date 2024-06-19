f4dd623b91c46ce421d64d2213a153ea0acaa632
AdhinN <ghanang.adhin@gmail.com>
2024-06-19 09:42:22 +0700

    idle player animation

--- Assets/Scripts/CharaController.cs
+++ Assets/Scripts/CharaController.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class CharaController : MonoBehaviour
 {
     private CharacterController _characterController;
+    private Animator _anim;
     [SerializeField] private float _speed;
 
     private void Start() 
     {
         _characterController = GetComponent<CharacterController>();    
+        _anim = GetComponent<Animator>();
     }
 
     private void Update() 
@@ -15,6 +17,16 @@ public class CharaController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
 
         Vector3 move = new Vector3(moveHorizontal, 0f, 0f);
-        _characterController.Move(move * _speed * Time.deltaTime);    
+
+        if(move != Vector3.zero)
+        {
+            _characterController.Move(move * _speed * Time.deltaTime);
+            _anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            _anim.SetBool("isMoving", false);
+        }
+        
     }
 }
